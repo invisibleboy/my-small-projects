6f0dda5bd2e7cc7e2a26405e91525ec422f78b04
ww345ww <ww345ww@gmail.com>
2012-04-17 08:47:59 +0000

    add controller benchmark

---
+++ controllers/Controller1_send/Controller0_data.c
@@ -0,0 +1,28 @@
+/*
+ * File: Controller0_data.c
+ *
+ * Real-Time Workshop code generated for Simulink model Controller0.
+ *
+ * Model version                        : 1.1934
+ * Real-Time Workshop file version      : 7.1  (R2008a)  23-Jan-2008
+ * Real-Time Workshop file generated on : Thu Feb 02 15:31:04 2012
+ * TLC version                          : 7.1 (Jan 18 2008)
+ * C/C++ source code generated on       : Thu Feb 02 15:31:04 2012
+ */
+
+#include "Controller0.h"
+#include "Controller0_private.h"
+
+/* Constant parameters (auto storage) */
+const ConstParam_Controller0 Controller0_ConstP = {
+  /* Expression: K
+   * '<S2>/Gain'
+   */
+  { -1.0222915002700748E+000, 3.7846225622656398E+001, 1.0407692270946861E+005,
+    -6.9424998176656536E+003, -2.0818534457119524E+007 }
+};
+
+/* File trailer for Real-Time Workshop generated code.
+ *
+ * [EOF]
+ */
