0d97a276f5bb7beba89c0cbe489d9380d267c41d
ww345ww <ww345ww@530ffb39-82d5-4eab-ec8d-f79e013eed8c>
2013-12-15 09:17:02 +0000

    After the final submission to DATE 2014

--- trunk/Shell/simulate.sh
+++ trunk/Shell/simulate.sh
@@ -1,9 +1,11 @@
 RUN=/home/qali/my-small-projects/Shell/run.sh
-PIN_DIR=/home/qali/Ins/pin-2.12-54730-gcc.4.4.7-linux
+PIN_DIR=/home/qali/Ins/pin-2.13-61206-gcc.4.4.7-linux
 PIN=$PIN_DIR/pin
 #PIN_TOOL=$PIN_DIR/source/tools/SimpleExamples/obj-ia32/pwlWriteDist.so
-#PIN_TOOL=$PIN_DIR/source/tools/SimpleExamples/obj-intel64/pwlStackEntry.so
-PIN_TOOL=$PIN_DIR/source/tools/SimpleExamples/obj-intel64/pwlBaseline.so
+PIN_TOOL=$PIN_DIR/source/tools/SimpleExamples/obj-intel64/pwlStackHeap.so
+#PIN_TOOL=$PIN_DIR/source/tools/SimpleExamples/obj-intel64/pwlBaseline.so
+#PIN_TOOL=$PIN_DIR/source/tools/ManualExamples/obj-intel64/malloctrace.so
+
 PIN_OPTION="-d $1"	
 	
 	
